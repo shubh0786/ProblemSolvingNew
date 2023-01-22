75a12074b1c50ce705ef602d29f0535a53307c76
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-01-22 23:39:09 +1300

    all

---
+++ Number2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Write a C# Sharp program that takes a number and a width also a number, as input and then displays a triangle of that width, using that number. 
+
+Console.WriteLine("Please Enter your Number");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write the width");
+int wid = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("");

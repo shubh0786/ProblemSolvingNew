8fd1b6dd23932ad0c00bd0367b738f8b18486c8b
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-02-12 23:57:27 +1300

    SOLVE

--- pro3/Program.cs
+++ pro3/Program.cs
@@ -12,6 +12,18 @@ namespace pro3
     {
         static void Main(string[] args)
         {
+            char letter, letter1, letter2;
+
+            Console.Write("Input letter: ");
+            letter = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input letter: ");
+            letter1 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input letter: ");
+            letter2 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}", letter2, letter1, letter);
         }
     }
 }

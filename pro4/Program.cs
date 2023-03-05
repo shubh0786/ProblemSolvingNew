883e20bc37107eca03cf5d941ffd3b4006d6f806
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-05 23:57:33 +1300

    problem added

--- pro4/Program.cs
+++ pro4/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Write a C# Sharp program to calculate profit and loss on a transaction.
 namespace pro4
 {
     internal class Program
